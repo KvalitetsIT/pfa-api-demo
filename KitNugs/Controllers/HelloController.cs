6200c62d6283ba59e62fcf6bc23a5200db5a9c29
Morten Matthiesen <mmb@kvalitetsit.dk>
2023-08-24 14:14:33 +0200

    Add IP to response

--- KitNugs/Controllers/HelloController.cs
+++ KitNugs/Controllers/HelloController.cs
@@ -19,10 +19,26 @@ namespace KitNugs.Controllers
             _logger.LogInformation("Entering GET!");
             var businessResult = await _helloService.BusinessLogic(name);
 
+            String ip;
+            var headers = Request.Headers.ToList();
+            if (headers.Exists((kvp) => kvp.Key == "X-Forwarded-For"))
+            {
+                // when running behind a load balancer you can expect this header
+                var header = headers.First((kvp) => kvp.Key == "X-Forwarded-For").Value.ToString();
+                // in case the IP contains a port, remove ':' and everything after
+                ip = header.Remove(header.IndexOf(':'));
+            }
+            else
+            {
+                // this will always have a value (running locally in development won't have the header)
+                ip = Request.HttpContext.Connection.RemoteIpAddress.ToString();
+            }
+
             return new HelloResponse { 
                 Now = businessResult.Now, 
                 Name = businessResult.Name,
-                From_configuration = businessResult.FromConfiguration
+                From_configuration = businessResult.FromConfiguration,
+                Ip = ip
             };
         }
     }
